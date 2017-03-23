c259e76df1627113e86ceb86d10d6b1c2eaa5d68
alex <lbhtran@gmail.com>
2017-03-23 14:53:13 +0000

    this is a grabber

---
+++ test.py
@@ -0,0 +1,20 @@
+import tweepy
+from tweepy import OAuthHandler
+
+consumer_key = 'yMRKHnT3x8Vyhp4rRY0zofBJA'
+consumer_secret = 'mNDHRyoSko64x9mIr241rOZcJvVRADKFkNmUZajv8lHRsD5VQS'
+access_token = '2378992174-YfR2egNwWGACPM93x1yOFdcm69XcLc6Qs6ecwlo'
+access_secret = 'GnOyHVgsifMMgfkTF1OQ7xUQ1D7JvHEP8h0xbXiFwt93c'
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
+
+#for status in tweepy.Cursor(api.home_timeline).items(10):
+    # Process a single status
+#    print(status.text)
+
+for status in tweepy.Cursor(api.home_timeline).items(10):
+    # Process a single status
+    process_or_store(status._json)
